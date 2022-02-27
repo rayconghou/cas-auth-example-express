397b9d74f24e2e6e596b259a8fa8c80c4e40f65f
wu-json <jason.wu2153@gmail.com>
2022-02-26 19:37:56 -0500

    feat: session

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import session from "express-session";
+
 import passport from "./passport";
 
 const app = express()
   .use(express.json())
-  .use(express.urlencoded({ extended: true }));
+  .use(express.urlencoded({ extended: true }))
+  .use(
+    session({
+      secret: "this is totally secret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
 
 passport(app);
 
