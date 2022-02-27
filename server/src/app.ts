a399de0384c7b0a3990854660e6b4fc8b6f21911
wu-json <jason.wu2153@gmail.com>
2022-02-26 20:28:55 -0500

    feat: cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,24 @@
+import cors from "cors";
 import express from "express";
 import session from "express-session";
-
+import { isCI, isDevelopment, isTest } from "./utils/environment";
 import passport from "./passport";
 
+const bypassCors = isCI() || isDevelopment() || isTest();
+const allowList = new Set(["http://localhost:3000"]);
+
+const corsOptions = {
+  origin: (origin: string, callback: any) => {
+    if (bypassCors || allowList.has(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 const app = express()
+  .use(cors(corsOptions))
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use(
